c76b06bfd355a39cdb1aef6154c95a00edc42af7
whyfcm@gmail.com <whyfcm@gmail.com>
2022-09-27 10:29:52 +0800

    belajar_ml

--- Fandescho.py
+++ Fandescho.py
@@ -1,1 +1,12 @@
-print('hello world')
+from flask import Flask
+app = Flask(__name__)
+@app.route('/Fandescho/hello-world')
+def api_hello_world():
+    first_name = "Fandescho"
+    last_name = "Malalantang"
+    age = 21
+    return  jsonify({
+        'firs_name' : first_name,
+        'last_name' : last_name,
+        'age' : age,
+    })

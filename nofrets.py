97c1b30bf8919789fbcd5b519e2ca0077a93b453
Nofrets Poai <opetstudio@gmail.com>
2022-09-27 10:26:48 +0800

    membuat api

--- nofrets.py
+++ nofrets.py
@@ -1,5 +1,29 @@
 from flask import Flask
+from flask import jsonify
+
 app = Flask(__name__)
-@app.route('/')
-def index():
-    return 'Norets Poai'
+@app.route('/nofrets/hello-world')
+def api_hello_world():
+    first_name = "Nofrets"
+    last_name = "Poai"
+    age = 17
+    return jsonify({
+        'first_name': first_name,
+        'last_name': last_name,
+        'age': age,
+        })
+
+@app.route('/nofrets/user')
+def api_user():
+    id = "123456789"
+    first_name = "Poai"
+    last_name = "Poai"
+    age = 17
+    email = 'nofrets.poai@gmail.com'
+    return jsonify({
+        'id': id,
+        'first_name': first_name,
+        'last_name': last_name,
+        'age': age,
+        'email': email,
+        })

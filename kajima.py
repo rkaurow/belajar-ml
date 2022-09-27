d4b895d1d6416edbf7d7a1057d0e17bc12ba5c7b
kajima-yv <vickoyudheakajima@gmail.com>
2022-09-27 10:11:49 +0800

    Flask with jsonify

--- kajima.py
+++ kajima.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello world!<p>"
+    message =  "<p>Hello world!<p>"
+    return jsonify({'message': message})

a6f940c81919d13131c09337010ebfcf77cd8c75
YonathanTjiptomo <ytjiptomo@gmail.com>
2022-09-27 10:28:26 +0800

    membuat api

--- Yonathan.py
+++ Yonathan.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Yonathan Tjiptomo</p>"
+@app.route('/yonathan/hello-world')
+def api_hello_world():
+    first_name = "Yonathan"
+    last_name = "Tjiptomo"
+    age = 21
+    return jsonify({
+        'first_name': first_name,
+        'last_name': last_name,
+        'age': age
+    })

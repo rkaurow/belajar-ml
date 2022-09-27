69605eaaf96d06d14e3ce5f0ae0c02002343afb4
Gracya Joicelin Lumenta <chenchenlumenta16@gmail.com>
2022-09-27 09:29:20 +0700

    membuat API

--- jordy.py
+++ jordy.py
@@ -1,1 +1,8 @@
+from flask import Flask
+from flask import jsonify
 
+app = Flask(__name__)
+@app.route('/jordy/index')
+def index():
+    message = "hello world"
+    return jsonify ({'message': message})

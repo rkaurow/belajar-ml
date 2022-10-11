ccc21d5957a48986853d805f68e5d2ddc34da833
Nofrets Poai <opetstudio@gmail.com>
2022-10-11 10:25:22 +0800

    add data set

--- nofrets.py
+++ nofrets.py
@@ -27,7 +27,7 @@ def api_user():
     # determined by the first position in the shape tuple, in this case 1.
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
     # Replace this with the path to your image
-    image = Image.open('nofrets.png').convert('RGB')
+    image = Image.open('agung.png').convert('RGB')
     #resize the image to a 224x224 with the same strategy as in TM2:
     #resizing the image to be at least 224x224 and then cropping from the center
     size = (224, 224)
